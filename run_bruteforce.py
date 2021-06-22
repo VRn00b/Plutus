71aafb470865d7b3637f80a65a811b4efc5c6ef6
swift502 <blaha.j502@gmail.com>
2021-06-22 21:42:35 +0200

    cloud prep

--- run_bruteforce.py
+++ run_bruteforce.py
@@ -16,30 +16,32 @@ def main(database):
 	while True:
 
 		# Mnemonic
-		words = bitcoinlib.mnemonic.Mnemonic().generate()
-		key = bitcoinlib.keys.HDKey().from_passphrase(words)
+		# words = bitcoinlib.mnemonic.Mnemonic().generate()
+		# key = bitcoinlib.keys.HDKey().from_passphrase(words)
 		
-		private = str(key)
-		public = key.public()
+		# private = str(key)
+		# public = key.public()
 		
-		plutus.process(private, public, key.address(), database, words)
-		plutus.process(private, public, key.address_uncompressed(), database, words)
+		# plutus.process(private, public, key.address(), database, words)
+		# plutus.process(private, public, key.address_uncompressed(), database, words)
 		
 		# Improved
 		key = newKeyGen.keygen_random()
+		print(key[3])
+		print(key[4])
 		plutus.process(key[0], key[2], key[3], database)
 		plutus.process(key[0], key[2], key[4], database)
 
 		# Original
-		private_key = oldKeyGen.generate_private_key()
-		public_key = oldKeyGen.private_key_to_public_key(private_key)
-		address = oldKeyGen.public_key_to_address(public_key)
-		plutus.process(private_key, public_key, address, database)
+		# private_key = oldKeyGen.generate_private_key()
+		# public_key = oldKeyGen.private_key_to_public_key(private_key)
+		# address = oldKeyGen.public_key_to_address(public_key)
+		# plutus.process(private_key, public_key, address, database)
 
 if __name__ == '__main__':
 	database = plutus.read_database()
 
 	# for cpu in range(multiprocessing.cpu_count()):
-	# for cpu in range(1):
-	# 	multiprocessing.Process(target=main, args=(database, )).start()
-	main(database)
+	for cpu in range(3):
+		multiprocessing.Process(target=main, args=(database, )).start()
+	# main(database)

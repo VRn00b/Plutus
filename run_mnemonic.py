c3cec28ac488282c7227a7675988fa1c063aa4ea
swift502 <blaha.j502@gmail.com>
2021-06-22 23:48:46 +0200

    run_mnemonic

--- run_mnemonic.py
+++ run_mnemonic.py
@@ -15,17 +15,16 @@ def main(database):
 	
 	while True:
 
-		# Improved
-		key = newKeyGen.keygen_random()
-		plutus.process(key[0], key[2], key[3], database)
-		plutus.process(key[0], key[2], key[4], database)
-
-		# Original
-		# private_key = oldKeyGen.generate_private_key()
-		# public_key = oldKeyGen.private_key_to_public_key(private_key)
-		# address = oldKeyGen.public_key_to_address(public_key)
-		# plutus.process(private_key, public_key, address, database)
-
+		# Mnemonic
+		words = bitcoinlib.mnemonic.Mnemonic().generate()
+		key = bitcoinlib.keys.HDKey().from_passphrase(words)
+		
+		private = str(key)
+		public = key.public()
+		
+		plutus.process(private, public, key.address(), database, words)
+		plutus.process(private, public, key.address_uncompressed(), database, words)
+		
 if __name__ == '__main__':
 	database = plutus.read_database()
 
